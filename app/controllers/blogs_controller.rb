eba7595ed7d0cffc5e1f0b8bf42908b24e4876cc
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-08 12:08:26 +0000

    Blog　削除機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -29,6 +29,11 @@ class BlogsController < ApplicationController
       render :edit
     end
   end
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path
+  end
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
